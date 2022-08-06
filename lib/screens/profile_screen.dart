22ef389560862094bf1fc2fd3f79286336e1f883
SelaseKay <judekwashie70@gmail.com>
2022-08-06 16:31:30 +0000

    complete profile screen ui

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,6 +13,12 @@ class ProfileScreen extends StatelessWidget {
             thickness: 1.5,
             color: Color(0xFFDDD3D0),
           ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(
+            padding: const EdgeInsets.only(
+                left: 0.0, top: 4.0, bottom: 4.0, right: 0.0),
+            side: const BorderSide(color: Color(0xFFEF5350)),
+          )),
           textTheme: const TextTheme(
               headline6: TextStyle(color: Color(0xFFA1887F)),
               subtitle1: TextStyle(
@@ -37,10 +43,13 @@ class ProfileScreen extends StatelessWidget {
                     const _ProfileHeader(),
                     const Padding(
                         padding: EdgeInsets.only(top: 40.0), child: Divider()),
-                    _ProfileItem(
-                      asset: "assets/images/mail.svg",
-                      itemText: "judekwashie70@gmail.com",
-                      style: Theme.of(context).textTheme.bodyText1,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24.0),
+                      child: _ProfileItem(
+                        asset: "assets/images/mail.svg",
+                        itemText: "judekwashie70@gmail.com",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
                     ),
                     const SizedBox(
                       height: 16.0,
@@ -62,20 +71,16 @@ class ProfileScreen extends StatelessWidget {
                       itemText: "Report History",
                       isArrowVisible: true,
                       isClickable: true,
-                       onPressed: (){},
+                      onPressed: () {},
                       style: Theme.of(context).textTheme.bodyText1,
                     ),
                     const SizedBox(
                       height: 8.0,
                     ),
-                     _ProfileItem(
-                      asset: "assets/images/log_out.svg",
-                      itemText: "Log out",
-                      isClickable: true,
-                      onPressed: (){},
-                      style:
-                          const TextStyle(fontSize: 16.0, color: Color(0xFFEF5350)),
-                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: _LogoutButton(onPressed: () {}),
+                    )
                   ],
                 ),
               ))
@@ -98,7 +103,7 @@ class _AppBarSection extends StatelessWidget {
         _IconButton(
           icon: Icons.arrow_back_ios_rounded,
           iconColor: Theme.of(context).primaryColor,
-          onPressed: () {},
+          onPressed: () => Navigator.of(context).pop(),
         ),
         Text(
           "Profile",
@@ -115,27 +120,22 @@ class _AppBarSection extends StatelessWidget {
 }
 
 class _IconButton extends StatelessWidget {
-  const _IconButton(
-      {Key? key,
-      required this.icon,
-      required this.iconColor,
-      this.onPressed,
-      this.height = 40.0,
-      this.width = 40.0})
-      : super(key: key);
+  const _IconButton({
+    Key? key,
+    required this.icon,
+    required this.iconColor,
+    this.onPressed,
+  }) : super(key: key);
   final IconData icon;
   final VoidCallback? onPressed;
-
-  final double height;
-  final double width;
 
   final Color iconColor;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: height,
-      width: width,
+      height: 40.0,
+      width: 40.0,
       child: Material(
         color: const Color(0xFFEFEFEF),
         borderRadius: const BorderRadius.all(Radius.circular(50.0)),
@@ -212,45 +212,68 @@ class _ProfileItem extends StatelessWidget {
     return Column(
       children: [
         !isClickable
-        ? Row(
-            children: [
-              SvgPicture.asset(asset),
-              const SizedBox(
-                width: 24.0,
-              ),
-              Text(
-                itemText,
-                style: style,
+            ? Row(
+                children: [
+                  SvgPicture.asset(asset),
+                  const SizedBox(
+                    width: 24.0,
+                  ),
+                  Text(
+                    itemText,
+                    style: style,
+                  )
+                ],
               )
-            ],
-          )
-        : TextButton(
-          style: TextButton.styleFrom(padding: const EdgeInsets.all(0.0)),
-          onPressed: onPressed,
-          child: Row(
-            children: [
-              SvgPicture.asset(asset),
-              const SizedBox(
-                width: 24.0,
-              ),
-              Expanded(
-                child: Text(
-                  itemText,
-                  style: style,
+            : TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(0.0),
+                  splashFactory: NoSplash.splashFactory,
                 ),
-              ),
-              isArrowVisible
-                  ? const _IconButton(
-                      icon: Icons.arrow_forward_ios_rounded,
-                      iconColor: Color(0xFF757575),
-                      height: 32.0,
-                      width: 32.0,
-                    )
-                  : const SizedBox.shrink()
-            ],
-          ),
-        )
+                onPressed: onPressed,
+                child: Row(
+                  children: [
+                    SvgPicture.asset(asset),
+                    const SizedBox(
+                      width: 24.0,
+                    ),
+                    Expanded(
+                      child: Text(
+                        itemText,
+                        style: style,
+                      ),
+                    ),
+                    isArrowVisible
+                        ? CircleAvatar(
+                            backgroundColor:
+                                const Color(0xFFE5A060).withOpacity(0.4),
+                            foregroundColor: null,
+                            radius: 16.0,
+                            child: const Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              color: Color(0xFFFFFFFF),
+                              size: 16.0,
+                            ),
+                          )
+                        : const SizedBox.shrink()
+                  ],
+                ),
+              )
       ],
     );
+  }
+}
+
+class _LogoutButton extends StatelessWidget {
+  const _LogoutButton({Key? key, required this.onPressed}) : super(key: key);
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+        onPressed: onPressed,
+        child: const Text(
+          "Log Out",
+          style: TextStyle(fontSize: 16.0, color: Color(0xFFEF5350)),
+        ));
   }
 }

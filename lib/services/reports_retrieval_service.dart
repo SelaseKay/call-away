f7527b6e8ba588371020b3bb6b9b0ffe1e055a12
SelaseKay <judekwashie70@gmail.com>
2022-08-29 11:48:26 +0000

    add getReports

--- lib/services/reports_retrieval_service.dart
+++ lib/services/reports_retrieval_service.dart
@@ -1,3 +1,6 @@
+import 'package:call_away/model/report.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class ReportRetrievalState {}
@@ -6,7 +9,11 @@ class ReportRetrievalStateInitial extends ReportRetrievalState {}
 
 class ReportRetrievalStateLoading extends ReportRetrievalState {}
 
-class ReportRetrievalStateSuccess extends ReportRetrievalState {}
+class ReportRetrievalStateSuccess extends ReportRetrievalState {
+  ReportRetrievalStateSuccess(this.reports);
+
+  final List<Report> reports;
+}
 
 class ReportRetrievalStateError extends ReportRetrievalState {
   ReportRetrievalStateError(this.errorMessage);
@@ -15,9 +22,27 @@ class ReportRetrievalStateError extends ReportRetrievalState {
 }
 
 class ReportRetrievalService extends StateNotifier<ReportRetrievalState> {
-  ReportRetrievalService(): super(ReportRetrievalStateInitial());
+  ReportRetrievalService() : super(ReportRetrievalStateInitial());
 
-  Future<void> getMyReports(){
-    
+  Future<void> getMyReports() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    state = ReportRetrievalStateLoading();
+
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .collection("reports")
+        .get()
+        .then((value) {
+      final jsonReports = value.docs.map((doc) => doc.data()).toList();
+
+      final reports =
+          jsonReports.map((report) => Report.fromJson(report)).toList();
+
+      state = ReportRetrievalStateSuccess(reports);
+    }).catchError((e) {
+      state = ReportRetrievalStateError(e.toString());
+    });
   }
 }

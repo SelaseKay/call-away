83000b7dc9e2fbe5b3a3f9b5e9713c40843f5fe4
SelaseKay <judekwashie70@gmail.com>
2022-09-26 10:20:40 +0000

    import unavailable packages

--- lib/services/notifications_service.dart
+++ lib/services/notifications_service.dart
@@ -1,17 +1,2 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
-class NotificationsService {
-  final _db = FirebaseFirestore.instance;
-
-  NotificationsService() {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    });
-  }
-}
